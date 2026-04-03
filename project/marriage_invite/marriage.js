2e143a7010db980ce7b83f901ee0e85e90a147cd
SuhwanMun <visvi518@gmail.com>
2026-04-03 16:44:52 +0900

    26.04.03 marriage invite 수정

--- project/marriage_invite/marriage.js
+++ project/marriage_invite/marriage.js
@@ -1,3 +1,4 @@
+//디데이 마이너스
 function getDday() {
             const targetDate = new Date("2026-11-29T00:00:00+09:00");
             const nowDate = new Date();
@@ -9,3 +10,11 @@ function getDday() {
 
         getDday();
         setInterval(getDday, 1000);
+
+//음악 자동재생
+function enableSound() {
+            var audio = document.getElementById("myAudio");
+            audio.muted = false; // 버튼 클릭 시 소리 켜기
+        }
+
+        
