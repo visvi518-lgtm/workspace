3b900d1f599ab5c0ab3494d1a4b1c390bee77a0a
SuhwanMun <visvi518@gmail.com>
2026-04-19 15:29:23 +0900

    전체적인 파일 정리, 파일 분류

--- project/marriage_invite/js/dDayMinus.js
+++ project/marriage_invite/js/dDayMinus.js
@@ -10,9 +10,3 @@ function getDday() {
 
         getDday();
         setInterval(getDday, 1000);
-
-//음악 자동재생
-function enableSound() {
-            const audio = document.getElementById("myAudio");
-            audio.muted = false; // 버튼 클릭 시 소리 켜기
-        }
---
+++ project/marriage_invite/js/musicAutoPlay.js
@@ -0,0 +1,5 @@
+//음악 자동재생
+function enableSound() {
+            const audio = document.getElementById("myAudio");
+            audio.muted = false; // 버튼 클릭 시 소리 켜기
+        }
