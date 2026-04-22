b704223fc9acc9327ff0a0899ed31cee4bd54bfa
SuhwanMun <visvi518@gmail.com>
2026-04-22 21:15:13 +0900

    nomad코더: todo list id 부여

--- nomadCoderStudy/momentum/js/todo.js
+++ nomadCoderStudy/momentum/js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+let toDos = [];
+
+const TODOS_KEY = "todos";
 
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteTodo(event){
@@ -14,17 +16,16 @@ function deleteTodo(event){
 }
 
 function paintToDo(newTodo){
-    console.log("i'll paint", newTodo);
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
-    console.log(li)
 }
 
 function handleToDoSubmit(event){
@@ -32,11 +33,26 @@ function handleToDoSubmit(event){
     console.log(toDoInput.value);
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
     console.log(newTodo, toDoInput.value)
 
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+
+
+if(savedToDos !==null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
